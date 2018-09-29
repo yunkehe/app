c9db31c1b7b27b1fd80f4235a2e19b5f73270031
yunkehe <yunkehe@163.com>
2018-09-29 18:16:54 +0800

    toast

---
+++ src/main/java/tk/yunheke/whereismycar/Location.java
@@ -0,0 +1,7 @@
+package tk.yunheke.whereismycar;
+
+public class Location {
+    static void main(String []args){
+        System.out.println("主方法入口：所有的Java 程序由public static void main(String []args)方法开始执行。");
+    }
+}
--- src/main/java/tk/yunheke/whereismycar/MainActivity.java
+++ src/main/java/tk/yunheke/whereismycar/MainActivity.java
@@ -6,18 +6,35 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//定位信息
+import android.location.LocationManager;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    private Button btnshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     //  将布局xml文件引入到activity中
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnshow = (Button) findViewById(R.id.btnshow);
+        btnshow.setOnClickListener(new OnClickListener(){
+//            重写点击事件的处理方法
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"你点击了按钮", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -41,4 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private LocationManager locationManager;
 }
